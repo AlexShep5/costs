f8bd2e00af426a3f8d55926e0b84083e2fea24fc
AlexShep5 <alxshep25@gmail.com>
2022-10-27 17:38:36 +0300

    lesson03

---
+++ lesson03/costs/src/components/PaymentsDisplay.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="list">
+        <div class="list__item">
+            <span class="list__number"><b>#</b></span>
+            <span class="list__date"><b>Date</b></span>
+            <span class="list__category"><b>Category</b></span>
+            <span class="list__value"><b>Value</b></span>
+        </div>
+        <div class="list__item" v-for="item, index in items" :key="index">
+            <span class="list__number">{{ index + 1 }}</span>
+            <span class="list__date">{{ item.date }}</span>
+            <span class="list__category">{{ item.type }}</span>
+            <span class="list__value">{{ item.amount }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PaymentsDisplay',
+    props: {
+        items: {
+            type: Array,
+            default() {
+                return []
+            }
+        }
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.list {
+    &__item {
+        height: 30px;
+        border-bottom: 1px solid #66CDAA;
+        display: flex;
+        align-items: center;
+    }
+
+    &__number {
+        width: 10%;
+    }
+
+    &__date {
+        width: 30%;
+    }
+
+    &__category {
+        width: 50%;
+    }
+
+    &__value {
+        width: 10%;
+    }
+}
+</style>
